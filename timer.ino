815f68558fecb2ccc8f2bb3628be7522641a1224
Alan Pinstein <apinstein@mac.com>
2019-05-18 19:58:51 -0400

    Add toggle between timer and stopwatch mode with joystick X

--- timer.ino
+++ timer.ino
@@ -2,13 +2,17 @@
 
 SevSeg sevseg;
 
+long countdownSeconds;
+long wantCountdownSeconds;
+long t0;
+long initialTimerSeconds = 20 * 60;
+
 void setup() {
   // This is the 4 Digit 7-Segment Display that comes with Lewis' arduino kit:
   //  Top Row:    1 A F  2 3 B
   //  Bottom Row: E D DP C G 4
   // It's a COMMON_CATHODE part
   // Wire it up w/resistors on the digit pin circuits
-
   byte numDigits = 4;
   byte digitPins[] = {2, 3, 4, 5};
   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
@@ -29,14 +33,9 @@ void setup() {
     disableDecPoint
   );
   sevseg.setBrightness(90);
-
-  resetTimer(20 * 60);
-  
   Serial.begin(115200);
+  resetTimer(initialTimerSeconds);
 }
-
-long countdownSeconds;
-long t0;
 
 // joystick
 const int X_pin = 0; // analog pin connected to X output
@@ -45,7 +44,6 @@ const int Y_pin = 1; // analog pin connected to Y output
 const int x_center = 495;
 const int y_center = 495;
 const int joystick_dead_zone = 100;
-
 
 enum run_modes { 
   setting_timer       = 1, 
@@ -72,6 +70,9 @@ void resetTimer(int t) {
   t0 = millis();
 }
 
+boolean isJoystickXNeutral(int x) {
+  return (x < x_center + joystick_dead_zone && x > x_center - joystick_dead_zone);
+}
 boolean isJoystickYNeutral(int y) {
   return (y < y_center + joystick_dead_zone && y > y_center - joystick_dead_zone);
 }
@@ -79,29 +80,34 @@ boolean isJoystickYNeutral(int y) {
 void loop() {
   int x, y;
   long minutes, seconds;
-  long wantCountdownSeconds;
 
   // monitor joystick for changes to timer setting
   x = analogRead(X_pin);
   y = 1023 - analogRead(Y_pin);
 
-  sprintf(line, "x = %d\ny = %d", x, y);
-  //Serial.println(line);
-
   // we activate joystick setting by moving it out of "center"
-  if (current_mode == running_timer && !isJoystickYNeutral(y)) {
+  if (current_mode != setting_timer && !isJoystickYNeutral(y)) {
       Serial.println("Entering setting mode");
       current_mode = setting_timer;
       wantCountdownSeconds = countdownSeconds;
       setting_delta_last_used_at_millis = 0;  // make sure it fires immediately once we enter this mode
       setting_mode_last_used_at_millis = millis();
-      
+  }
+  if (current_mode != setting_timer && !isJoystickXNeutral(x)) {
+    if (x > x_center) {
+      current_mode = running_stopwatch;
+      resetTimer(0);
+    } else if (x < x_center) {
+      current_mode = running_timer;
+      resetTimer(wantCountdownSeconds);
+    }
   }
 
   sprintf(line, "current_mode: %d\n", current_mode);
   //Serial.println(line);
   
   signed long deltaS = 0;
+  long secondsElapsed, secondsLeft;
   switch (current_mode) {
     case setting_timer:
       if (!isJoystickYNeutral(y)) {
@@ -163,18 +169,27 @@ void loop() {
       } else {
         setting_mode_last_used_at_millis = millis();
       }
-      //delay(100);
       break;
       
     case running_timer:
-      long secondsElapsed = (millis() - t0) / 1000;
-      long secondsLeft = countdownSeconds - secondsElapsed;
+      secondsElapsed = (millis() - t0) / 1000;
+      secondsLeft = countdownSeconds - secondsElapsed;
       minutes = secondsLeft / 60;
       seconds = secondsLeft % 60;
       
       sevseg.setNumber(minutes * 100 + seconds, 2);
       sevseg.refreshDisplay();
       break;
-  }
 
+        
+    case running_stopwatch:
+      secondsElapsed = (millis() - t0) / 1000;
+      
+      minutes = secondsElapsed / 60;
+      seconds = secondsElapsed % 60;
+      
+      sevseg.setNumber(minutes * 100 + seconds, 2);
+      sevseg.refreshDisplay();
+      break;
+  }
 }
